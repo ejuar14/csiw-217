0b657bdbe3273850a35833aeae9704e16ef06c8a
ejuar14 <ejuarezy12@gmail.com>
2026-09-01 17:40:17 -0600

    Update HW1.sql

--- week01/HW1.sql
+++ week01/HW1.sql
@@ -3,4 +3,4 @@ select version();
 select 'Hello, SQL!';
 create database analysis;
 create table teachers (id bigserial, hire_date date);
-why semicolons are imprtant is becoues that is to show were the command ends. 
+-- why semicolons are imprtant is becoues that is to show were the command ends. 

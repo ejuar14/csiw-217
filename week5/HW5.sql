c79c2b3f12512743db8dfd0e3915655cd83bfdcf
ejuar14 <ejuarezy12@gmail.com>
2026-09-15 21:02:37 -0600

    Update HW5.sql

--- week5/HW5.sql
+++ week5/HW5.sql
@@ -16,3 +16,50 @@ ALTER TABLE books ADD COLUMN genre text;
 ALTER TABLE books ADD COLUMN price numeric;
 ALTER TABLE books ADD COLUMN last_updated timestamp;
 ALTER TABLE books ADD COLUMN in_stock boolean;
+
+UPDATE books
+SET genre = 'Young Adult Dystopian Science Fiction',
+price = 39.99,
+last_updated = now(),
+in_stock = TRUE
+WHERE book_name = 'The Maze Runner';
+
+UPDATE books
+SET genre = 'allegorical novel',
+price = 30.00,
+last_updated = '20260812',
+in_stock = False
+WHERE book_name = 'Lord of the Flies';
+
+UPDATE books
+SET genre = 'Young Adult',
+price = 25.00,
+last_updated = now(),
+in_stock = False
+WHERE book_name = 'The Outsiders';
+
+UPDATE books
+SET genre = 'Bildungsroman and Southern Gothic',
+price = 35.00,
+last_updated = '20260525',
+in_stock = True
+WHERE book_name = 'To Kill a Mockingbird';
+
+UPDATE books
+SET genre = 'Horror',
+price = 20.00,
+last_updated = now(),
+in_stock = True
+WHERE book_name = 'The Shadow over Innsmouth';
+
+ALTER TABLE books
+RENAME COLUMN price TO rating;
+
+ALTER TABLE book
+RENAME COLUMN rating TO price;
+
+ALTER TABLE books
+RENAME COLUMN price TO rating;
+
+SELECT * FROM books
+SELECT book_name FROM books

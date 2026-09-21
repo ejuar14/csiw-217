db2a526b8e72f6f0ea4d352290ba01fad352f935
ejuar14 <ejuarezy12@gmail.com>
2026-09-21 11:33:00 -0600

    Create week6lab.sql

---
+++ week6/week6lab.sql
@@ -0,0 +1,6 @@
+CREATE database week6
+create table product (product_index integer primary key,product_name text, description text , brand text, caregory text, price numeric (10,2),currency varchar (20), stock integer, EAN text, color text, product_size varchar(25), availability text, internal_id integer)
+SELECT * FROM product LIMIT 5
+SELECT product_name, price*stock FROM product
+SELECT AVG(price) FROM product
+SELECT MIN(price), MAX(price) FROM product

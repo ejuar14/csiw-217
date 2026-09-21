c3cada8e904fdc795259556451efe2f37c968752
ejuar14 <ejuarezy12@gmail.com>
2026-09-21 11:34:56 -0600

    Update week6lab.sql

--- week6/week6lab.sql
+++ week6/week6lab.sql
@@ -1,4 +1,4 @@
-CREATE database week6
+CREATE DATABASE week6
 create table product (product_index integer primary key,product_name text, description text , brand text, caregory text, price numeric (10,2),currency varchar (20), stock integer, EAN text, color text, product_size varchar(25), availability text, internal_id integer)
 SELECT * FROM product LIMIT 5
 SELECT product_name, price*stock FROM product

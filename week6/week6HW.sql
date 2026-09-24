09d56c55784fa9beb4b9ce1644b9d5762c446928
ejuar14 <ejuarezy12@gmail.com>
2026-09-23 19:28:11 -0600

    Update week6HW.sql

--- week6/week6HW.sql
+++ week6/week6HW.sql
@@ -1,2 +1,10 @@
+CREATE DATABASE week6homework;
 CREATE TABLE offers(id integer primary key,ean varchar(35),stock integer, price numeric(10,2));
 SELECT SUM(price*stock) AS stock_sold FROM offers;
+SELECT AVG(price) AS price_per_stock FROM offers;
+SELECT * FROM offers WHERE price > 500;
+SELECT COUNT(*) FROM offers GROUP BY stock;
+SELECT price, stock FROM offers ORDER BY price ASC;
+
+-- New concept that i have noticed is JOIN.
+-- How often do you normally join tables together?
